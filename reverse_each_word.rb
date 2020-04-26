0125b4586f393f081f4e1dded4aa12c55b051eeb
Matt Guttman <msguttman@gmail.com>
2020-04-26 14:17:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,8 @@ def reverse_each_word(string)
   
   array_of_words = string.split
   array_of_words.each { |i|
-  i.reverse
-  reversed_array_of_words << i
+  ii = i.reverse
+  reversed_array_of_words << ii
   }
   puts reversed_array_of_words
   final_string = reversed_array_of_words.join(" ")
