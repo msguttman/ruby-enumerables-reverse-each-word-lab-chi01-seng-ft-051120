12d3a277601924423b54c615663f0ea1bc996961
Matt Guttman <msguttman@gmail.com>
2020-04-26 14:20:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,16 @@
 
 
 def reverse_each_word(string)
-  reversed_array_of_words = []
+#  reversed_array_of_words = []
+#  
+#  array_of_words = string.split
+#  array_of_words.each { |i|
+#  ii = i.reverse
+#  reversed_array_of_words << ii
+#  }
+#  puts reversed_array_of_words
+#  final_string = reversed_array_of_words.join(" ")
   
-  array_of_words = string.split
-  array_of_words.each { |i|
-  ii = i.reverse
-  reversed_array_of_words << ii
-  }
-  puts reversed_array_of_words
-  final_string = reversed_array_of_words.join(" ")
   
   
 end
