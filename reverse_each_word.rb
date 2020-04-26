b0ca4ff15cae31a4985c04a279094ef406a1a504
Matt Guttman <msguttman@gmail.com>
2020-04-26 14:18:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,4 @@
 
-my_name = "matt"
-puts my_name.reverse
-
 
 def reverse_each_word(string)
   reversed_array_of_words = []
@@ -15,4 +12,3 @@ def reverse_each_word(string)
   final_string = reversed_array_of_words.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
